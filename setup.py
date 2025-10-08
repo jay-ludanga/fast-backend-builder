59a6e0d02f047f8b5d68957eb0a5d0c858fd4ae8
Japhary <jjuma@mzumbe.ac.tz>
2025-10-08 11:27:29 +0300

    add in comparator

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fast-backend-builder',
-    version='0.1.3.19',
+    version='0.1.3.20',
     packages=find_packages(),
     install_requires=[
         'fastapi==0.116.1',
