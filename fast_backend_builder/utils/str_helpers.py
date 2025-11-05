79fd4b637efe420075a467b9ed69eadbde4af601
Japhary Juma <jjuma@mzumbe.ac.tz>
2025-11-05 14:43:11 +0300

    minor imprv notification

--- fast_backend_builder/utils/str_helpers.py
+++ fast_backend_builder/utils/str_helpers.py
@@ -1,5 +1,5 @@
-
 import re
+
 
 def to_snake_case(name):
     """
@@ -15,14 +15,15 @@ def to_snake_case(name):
     - Input: 'CamelCase' or 'PascalCase'
     - Output: 'camel_case' or 'pascal_case'
     """
-    
+
     # Use a regular expression to find positions where an uppercase letter follows a lowercase letter or another uppercase letter,
     # but not at the beginning of the string (indicated by the negative lookbehind '(?<!^)').
     # The regex replaces the matched position with an underscore ('_') before the uppercase letter.
     name = re.sub(r'(?<!^)(?=[A-Z])', '_', name)
-    
+
     # Convert the entire string to lowercase to match snake_case formatting
     return name.lower()
+
 
 def to_kebab_case(name):
     """
@@ -38,14 +39,15 @@ def to_kebab_case(name):
     - Input: 'CamelCase' or 'PascalCase'
     - Output: 'camel_case' or 'pascal_case'
     """
-    
+
     # Use a regular expression to find positions where an uppercase letter follows a lowercase letter or another uppercase letter,
     # but not at the beginning of the string (indicated by the negative lookbehind '(?<!^)').
     # The regex replaces the matched position with an underscore ('_') before the uppercase letter.
     name = re.sub(r'(?<!^)(?=[A-Z])', '-', name)
-    
+
     # Convert the entire string to lowercase to match snake_case formatting
     return name.lower()
+
 
 def to_title_case(snake_str: str) -> str:
     """
@@ -62,7 +64,7 @@ def to_title_case(snake_str: str) -> str:
     """
     if not isinstance(snake_str, str):
         raise ValueError("Input must be a string.")
-    
+
     if not snake_str:
         return ''
 
@@ -71,12 +73,14 @@ def to_title_case(snake_str: str) -> str:
         words = snake_str.split('_')
     elif snake_str.find("-") != -1:
         words = snake_str.split('-')
-    else: words = snake_str.split(' ')
-    
+    else:
+        words = snake_str.split(' ')
+
     # Capitalize and join words into Title Case format
     title_case_str = ' '.join(word.capitalize() for word in words if word)
-    
+
     return title_case_str
+
 
 def remove_between(str, start, end, inclusive=True):
     """
@@ -93,17 +97,18 @@ def remove_between(str, start, end, inclusive=True):
     Returns:
     - str: The modified string with the specified content removed.
     """
-    
+
     if inclusive:
         # Pattern to match and remove everything between 'start' and 'end', including them
         pattern = re.escape(start) + ".*?" + re.escape(end)
     else:
         # Pattern to remove only the content between 'start' and 'end', keeping the substrings themselves
         pattern = re.escape(start) + "(.*?)" + re.escape(end)
-    
+
     # Use re.sub to replace the matched pattern with an empty string or with the preserved start/end strings
     # Use re.DOTALL to ensure multi-line support
     return re.sub(pattern, '' if inclusive else start + end, str, flags=re.DOTALL)
+
 
 def to_strptime_format(date_format: str) -> str:
     # Dictionary mapping placeholders without % to those with %
@@ -116,12 +121,13 @@ def to_strptime_format(date_format: str) -> str:
         'M': '%M',  # Minute (00-59)
         'S': '%S',  # Second (00-59)
     }
-    
+
     # Replace each placeholder in the input format with the appropriate % placeholder
     for key, value in format_map.items():
         date_format = date_format.replace(key, value)
-    
+
     return date_format
+
 
 def is_valid_email(email: str) -> bool:
     """
@@ -135,6 +141,7 @@ def is_valid_email(email: str) -> bool:
     """
     email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     return re.match(email_regex, email) is not None
+
 
 def is_valid_url(url: str) -> bool:
     """
@@ -154,6 +161,7 @@ def is_valid_url(url: str) -> bool:
     )
     return re.match(url_regex, url) is not None
 
+
 def to_shorter_name(name, max_length=31):
     """
     Shortens a worksheet name to ensure it does not exceed the specified length 
@@ -170,21 +178,27 @@ def to_shorter_name(name, max_length=31):
     # If the name is already within the allowed length, return it as is
     if len(name) <= max_length:
         return name
-    
+
     # Split the name into words
     words = name.split()
     result = []
-    
+
     # Add words one by one until the total length exceeds the limit
     for word in words:
         if len(" ".join(result + [word])) <= max_length:
             result.append(word)
         else:
             break
-    
+
     # If no words could fit and the first word is too long, truncate it
     if not result and len(words[0]) > max_length:
         return words[0][:max_length]
-    
+
     # Join the collected words into a single string and return
     return " ".join(result)
+
+
+def normalize_model_name(name: str) -> str:
+    # Insert a space before capital letters (except the first one)
+    normalized = re.sub(r'(?<!^)(?=[A-Z])', ' ', name)
+    return normalized
